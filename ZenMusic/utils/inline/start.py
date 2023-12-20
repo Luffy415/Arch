1f052e55ff754c06be9e485a402f11270107b169
ᴀᴅɪᴛʏᴀ ᴊʜᴀ <119198739+PyAaditya@users.noreply.github.com>
2023-12-20 17:24:24 +0530

    Update start.py

--- ZenMusic/utils/inline/start.py
+++ ZenMusic/utils/inline/start.py
@@ -100,7 +100,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     if OWNER:
         buttons.append(
             [
-#                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
+                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
 #                InlineKeyboardButton(
 #                    text=_["S_B_6"], url=f"https://github.com/ArchBots/ZenMusic"
 #                ),
