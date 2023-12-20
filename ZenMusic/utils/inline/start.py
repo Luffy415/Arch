a1525eb9a531f7f901fe663654d331ecebfcb0ac
ᴀᴅɪᴛʏᴀ ᴊʜᴀ <119198739+PyAaditya@users.noreply.github.com>
2023-12-20 12:43:42 +0530

    Update start.py

--- ZenMusic/utils/inline/start.py
+++ ZenMusic/utils/inline/start.py
@@ -56,7 +56,10 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             InlineKeyboardButton(
                 text=_["S_B_5"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
+                    ),
+"""            InlineKeyboardButton(
+                text=_["S_B_8"], callback_data="settings_back_helper",
+),"""
         ]
     ]
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
@@ -94,11 +97,11 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             )
         ]
     )
-    if OWNER:
+"""    if OWNER:
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-#                InlineKeyboardButton(
+"""#                InlineKeyboardButton(
 #                    text=_["S_B_6"], url=f"https://github.com/ArchBots/ZenMusic"
 #                ),
             ]
@@ -112,12 +115,12 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                     ),
                 ]
             )
-        if OWNER:
+"""        if OWNER:
             buttons.append(
                 [
                     InlineKeyboardButton(
                         text=_["S_B_7"], user_id=OWNER
                     ),
                 ]
-            )
+            )"""
     return buttons
